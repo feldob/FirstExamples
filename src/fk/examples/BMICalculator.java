b2df9d0607537b97b0dc1686e3e9955de11d6316
Felix Dobslaw <felix.dobslaw@miun.se>
2024-03-17 14:55:47 +0100

    a solution to assignment 1.

--- src/fk/examples/BMICalculator.java
+++ src/fk/examples/BMICalculator.java
@@ -1,30 +1,44 @@
 package fk.examples;
+
 public class BMICalculator {
 
 	public static void main(String[] args) {
-		// Set weight and height
-		double weight = 170; // in kilograms
-		double height = 1.75; // in meters
 
-		String bmiCategory = calculateBMICategory(weight, height);
-		System.out.println("Your BMI category is: " + bmiCategory);
+		Person[] persons = new Person[2];
+		persons[0] = new Person("Skeletor", 3, 1.90);
+		persons[1] = new Person("Jabba The Hut", 1433, 3.60);
+
+		for (Person person : persons) {
+			bmiStatusToString(person);
+		}
 	}
 
 	public static String calculateBMICategory(double weight, double height) {
 		double bmi = calculateBMI(weight, height);
+		return getBMICategoryFor(bmi);
+	}
 
+	private static String getBMICategoryFor(double bmi) {
 		if (bmi < 18.5) {
 			return "Underweight";
 		} else if (bmi >= 18.5 && bmi < 25) {
 			return "Normal weight";
 		} else if (bmi >= 25 && bmi < 30) {
 			return "Overweight";
-		} else {
+		} else if (bmi >= 30 && bmi < 40) {
 			return "Obese";
+		} else {
+			return "Severely Obese";
 		}
 	}
 
 	public static double calculateBMI(double weight, double height) {
 		return weight / (height * height);
+	}
+
+	private static void bmiStatusToString(Person person) {
+		double bmi = calculateBMI(person.weight, person.height);
+		String category = getBMICategoryFor(bmi);
+		System.out.println(person.name + " is " + category + ".");
 	}
 }
---
+++ src/fk/examples/Person.java
@@ -0,0 +1,16 @@
+package fk.examples;
+
+public class Person {
+
+	String name;
+	
+	double weight;
+	
+	double height;
+	
+	public Person(String name, double weight, double height) {
+		this.name = name;
+		this.weight = weight;
+		this.height = height;
+	}
+}
